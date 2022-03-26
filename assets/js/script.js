a2a7fc67ec9adbf50ea0f6efc4b31bd714754da4
kwalczak80 <christopher.walczak@gmail.com>
2022-03-26 13:42:20 +0000

    Adding sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
 let user = document.getElementById('playername');
 let home = document.getElementById('home');
 let form = document.getElementById('player-form');
-form.addEventListener('submit', function(event){
-    home.style.display = 'none';
+let rules = document.getElementById('quiz-rules');
+
+home.addEventListener('submit', function(event){
+    this.style.display = 'none';    
+    rules.classList.remove('hide-content');
     console.log('Username', user.value);
+    console.log(rules);
     event.preventDefault();
 });
+
